88ad81cf497f7c5db568f638f1a8e9f67f8e4a2a
Anju Ito <anjuannito@gmail.com>
2023-03-18 18:44:32 -0400

    TB.sv modified for compilation errors

--- fpga/TB.sv
+++ fpga/TB.sv
@@ -2,28 +2,26 @@ module TB;
   logic clock, resetN;
 
   logic [6:0] HEX_D[6];
-  logic [3:0] gpio;
-  logic [17:0] ledr;
+  wire [35:0] GPIO_0, GPIO_1;
+  wire [17:0] ledr;
 
   ChipInterface dut (
     .CLOCK_50(clock),
-    .GPIO_1_D14(gpio[0]),
-    .GPIO_1_D15(gpio[1]),
-    .GPIO_1_D16(gpio[2]),
-    .GPIO_1_D17(gpio[3]),
+    .GPIO_0,
+    .GPIO_1,
     .SW({ 9'b0, 1'b1 }),
     .KEY({ 3'b1, resetN }),
     .LEDR(ledr),
     .HEX5(HEX_D[5]),
     .HEX4(HEX_D[4]),
+    .HEX3(HEX_D[3]),
+    .HEX2(HEX_D[2]),
     .HEX1(HEX_D[1]),
-    .HEX0(HEX_D[0]),
-    .GPIO_0_D14(gpio[0]),
-    .GPIO_0_D15(gpio[1]),
-    .GPIO_0_D16(gpio[2]),
-    .GPIO_0_D17(gpio[3])
+    .HEX0(HEX_D[0])
 );
 
+  assign GPIO_0[26] = GPIO_0[20];
+  assign GPIO_0[32] = GPIO_0[16];
 
   initial begin
     clock = 1'b0;
